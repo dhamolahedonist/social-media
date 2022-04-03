65054fc0f6b1aecbcada48eac2946d144ce6328f
Samuel Oduba <samueloduba123@gmail.com>
2022-04-02 18:26:13 -0700

    Initial application commit

---
+++ db.js
@@ -0,0 +1,50 @@
+const mysql = require('mysql2');
+
+const DB = {
+    init() {
+        return mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            port: '3306',
+            database: 'socialmedia' 
+        })
+    },
+
+    async get() {
+        const value = await DB.init().promise().query(`select * from users`)
+        return value[0]
+    },
+    create(payload) {
+        const { first_name, last_name, email, phone, twitter_handle, instagram_handle, password } = payload
+
+        DB.init().promise().query(`
+        INSERT INTO users 
+        (first_name, last_name, email, phone, twitter_handle, instagram_handle, password)
+        VALUES ('${first_name}', '${last_name}', '${email}', '${phone}', '${twitter_handle}', '${instagram_handle}', '${password}')
+        `)
+    },
+
+    async emailAlreadyExist(email) {
+        const value = await DB.init().promise().query(`select email from users where email = '${email}'`)
+        value[0].length > 0
+    },
+
+    async phoneAlreadyExist(phone) {
+        const value = await DB.init().promise().query(`select phone from users where phone = '${phone}'`)
+        return value[0].length > 0
+    },
+
+    async twitterHandleAlreadyExist(twitter_handle) {
+        const value = await DB.init().promise().query(`select twitter_handle from users where twitter_handle = '${twitter_handle}'`)
+        return value[0].length > 0
+    },
+
+     async instagramHandleAlreadyExist(instagram_handle) {
+        const value = await DB.init().promise().query(`select instagram_handle from users where instagram_handle = '${instagram_handle}'`)
+        return value[0].length > 0
+     }    
+
+}
+
+module.exports = DB;
---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const Joi = require('joi');
+const db = require('./db');
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    return response.json({
+        status: 'Successful',
+        message: 'User registered successfully',
+        data: []
+    })
+})
+
+app.get('/api/get-user-details', async (request, response) => {
+    const userDetails = await db.get()
+    return response.json({
+        status: 'Successful',
+        message: 'User registered successfully',
+        data: userDetails
+    })
+})
+
+app.post('/api/register-account', async (request, response) => {
+    const schema = Joi.object({
+        first_name: Joi.string().required(),
+        last_name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        phone: Joi.string().length(11).pattern(/^[0-9]+$/).required(),
+        twitter_handle: Joi.string().min(5).required(),
+        instagram_handle: Joi.string().min(5).required(),
+        password: Joi.string().regex(/^[\x20-\x7E]+$/).min(8).max(72).required(),   
+        confirm_password:Joi.string().required().valid(Joi.ref('password')),
+    })
+
+    const validation = schema.validate(request.body)
+    if(validation.error && validation.error.details.length > 0) {
+        return response.json ({
+            status: 'Failed',
+            message: validation.error.details[0].message,
+            data: null
+        }, 400)
+    }
+
+    const phoneNumberExist = await db.phoneAlreadyExist(request.body.phone)
+    const emailAddressExist = await db.emailAlreadyExist(request.body.email)
+    const twitterHandleExist = await db.twitterHandleAlreadyExist(request.body.twitter_handle)
+    const instagramHandleExist = await db.instagramHandleAlreadyExist(request.body.instagram_handle)
+
+    if(phoneNumberExist) {
+        return response.json ({
+            status: 'failed',
+            message: 'phone number already exist',
+            data: null
+        }, 400)
+    }
+    if(emailAddressExist) {
+        return response.json ({
+            status: 'failed',
+            message: 'email already exist',
+            data: null
+        }, 400)
+    }
+    if(twitterHandleExist) {
+        return response.json ({
+            status: 'failed',
+            message: 'Twitter handle already exist',
+            data: null
+        }, 400)
+    }
+
+      if(instagramHandleExist) {
+        return response.json ({
+            status: 'failed',
+            message: 'Instagram handle already exist',
+            data: null
+        }, 400)
+    }
+    db.create(request.body)
+
+    return response.json({
+        status: 'successful',
+        message: 'user registered successfully',
+        data: null
+    })
+
+})
+const   port = 3000
+console.log(`everything go dy alright laslas ${port}`)
+app.listen(port)
